bfbce6ac534b959f40320e6ff6ef349dae8d5128
Jesus Ramon <108687344+JesusRamon2192@users.noreply.github.com>
2024-04-02 03:57:15 +0000

    Clase 35 ngOnChanges

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
 })
 export class CounterComponent {
-  @Input({ required: true }) duration: number = 0;
-  @Input({ required: true }) message: string = '';
+  @Input({ required: true }) duration = 0;
+  @Input({ required: true }) message = '';
 
   constructor() {
     // NO ASYNC
