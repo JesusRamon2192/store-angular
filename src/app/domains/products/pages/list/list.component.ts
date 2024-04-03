6f9f2ac32b0b9cb6780d168709c9e0c9de20bdd4
Jesus Ramon <108687344+JesusRamon2192@users.noreply.github.com>
2024-04-03 02:26:32 +0000

    Clase 41 Creando componente de productos

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../../components/product/product.component';
+import { Product } from './../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,34 @@ import { ProductComponent } from './../../components/product/product.component';
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=42',
+        creationAt: new Date().toISOString(),
+      },
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string) {
     console.log('estamos en el padre');
     console.log(event);
