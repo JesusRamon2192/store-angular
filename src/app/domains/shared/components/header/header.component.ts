ec212d85df8a91bd28bb092dad98b3a1e303e059
JesusRamon2192 <jesus.ramon2192@gmail.com>
2024-04-04 13:55:06 -0600

    Clase47 Reactividad con signals en servicios

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component, signal, Input,SimpleChanges } from '@angular/core';
+import { Component, signal, Input,SimpleChanges, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from './../../../shared/models/product.model';
 import { ProductComponent } from '../../../products/components/product/product.component';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -12,23 +13,11 @@ import { ProductComponent } from '../../../products/components/product/product.c
 })
 export class HeaderComponent {
   hideSideMenu = signal(true);
-  @Input({ required: true }) cart: Product[] = [];
-  total=signal(0);
+  private cartService = inject(CartService);
+  cart = this.cartService.cart;
+  total = this.cartService.total;
 
   toggleSideMenu() {
     this.hideSideMenu.update((prevState) => !prevState);
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
-    //Add '${implements OnChanges}' to the class.
-    const cart = changes['cart'];
-    if (cart){
-      this.total.set(this.calcTotal());
-    }
-  }
-
-  calcTotal(){
-    return this.cart.reduce((total, product)=> total+product.price, 0);
   }
 }
--- src/app/domains/shared/services/cart.service.ts
+++ src/app/domains/shared/services/cart.service.ts
@@ -1,11 +1,15 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, computed, signal } from '@angular/core';
 import { Product } from '../models/product.model';
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  cart = signal<Product[]>([])
+  cart = signal<Product[]>([]);
+  total = computed(()=> {
+    const cart = this.cart();
+    return cart.reduce((total, product)=> total+product.price, 0);
+  })
   
   constructor() { }
   addToCart(product: Product){
