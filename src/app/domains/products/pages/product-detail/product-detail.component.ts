5d7341ebf5b4a5ee38fdf7b577e1ef56bb260a9a
Jesus Ramon <108687344+JesusRamon2192@users.noreply.github.com>
2024-04-06 02:42:09 +0000

    Clase 63 Detalle de la galeria

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '@shared/services/product.service';
 import { Product } from '@shared/models/product.model';
+import { CartService } from '@shared/services/cart.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -15,6 +16,7 @@ export class ProductDetailComponent {
   product = signal<Product | null>(null);
   cover = signal('');
   private productService = inject(ProductService);
+  private cartService = inject(CartService);
 
   ngOnInit() {
     if (this.id) {
@@ -31,5 +33,11 @@ export class ProductDetailComponent {
   }
   changeCover(newImg: string) {
     this.cover.set(newImg);
+  }
+  addToCart() {
+    const product = this.product();
+    if (product) {
+      this.cartService.addToCart(product);
+    }
   }
 }
